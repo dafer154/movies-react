f2d9bc4e3da3b1e85dae89453ecd402911e0ce55
David Zuluaga <david_fza@hotmail.com>
2020-03-10 16:00:28 -0500

    Updates

---
+++ precache-manifest.5296e2a986a12b08f57b6443d2114272.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d44c806951791d7016c5b55fda3887",
+    "url": "/movies-react/index.html"
+  },
+  {
+    "revision": "37fc1d0f186a52eda179",
+    "url": "/movies-react/static/css/main.d20d7e65.chunk.css"
+  },
+  {
+    "revision": "b5338a05570cb345b842",
+    "url": "/movies-react/static/js/2.0f066256.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/movies-react/static/js/2.0f066256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37fc1d0f186a52eda179",
+    "url": "/movies-react/static/js/main.264c6167.chunk.js"
+  },
+  {
+    "revision": "a29f7b474ff95ffbda37",
+    "url": "/movies-react/static/js/runtime-main.40aed147.js"
+  }
+]);
